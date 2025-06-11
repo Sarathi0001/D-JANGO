541ea39b311f06b651ff3cb59fbd5d7df3666f26
SARATHI  M <113877527+Sarathi0001@users.noreply.github.com>
2025-06-11 14:51:42 +0530

    Add files via upload

---
+++ Saran_Redimates/Saran_Redimates/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Saran_Redimates project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Saran_Redimates.settings')
+
+application = get_asgi_application()
